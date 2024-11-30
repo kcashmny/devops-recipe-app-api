a805a65ffc28b6a3d938da5b67f8222e2d07f618
kcashmny <kevincashwell@gmail.com>
2024-11-30 09:29:03 -0600

    added terraform deploy

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "The prefix to use for all resources"
+  default     = "raa"
+}
+
+variable "project" {
+  description = "The name of the project"
+  default     = "recipe-app-api"
+}
+
+variable "contact" {
+  description = "The contact person for the project"
+  default     = "kevincashwell@example.com"
+}
