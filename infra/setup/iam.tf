d2a3066205cd6b0768fd205808f9501675372930
kcashmny <kevincashwell@gmail.com>
2026-03-14 14:32:18 -0500

    update

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -71,7 +71,8 @@ data "aws_iam_policy_document" "ecr" {
       "ecr:UploadLayerPart",
       "ecr:InitiateLayerUpload",
       "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage"
+      "ecr:PutImage",
+      "ecr:BatchGetImage"
     ]
     resources = [
       aws_ecr_repository.app.arn,
